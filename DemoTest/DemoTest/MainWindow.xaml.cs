ff7dc2f647b7b8e1132f4e3d33ccfcb90588d219
surprise3465 <spirit3465@sina.com>
2017-07-08 15:21:41 +0800

    2017-7-8

--- DemoTest/DemoTest/MainWindow.xaml.cs
+++ DemoTest/DemoTest/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace DemoTest
 {
@@ -21,15 +22,40 @@ namespace DemoTest
     public partial class MainWindow : Window
     {
         private String ipaddr = "";
+        private DispatcherTimer timer1 = new DispatcherTimer();
         public MainWindow()
         {
             InitializeComponent();
-            SetIpWin window1 = new SetIpWin();
-            if(window1.ShowDialog() == true)
+            SetIpWin window = new SetIpWin();
+            if(window.ShowDialog() == true)
             {
-                ipaddr = window1.ipaddr;
-                return;
+                ipaddr = window.ipaddr;
+                ;
             }
+            timer1.Interval = TimeSpan.FromSeconds(1);
+            timer1.Tick += timer1_Tick;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //（你的定时处理）
+            datatime.Text = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            int utchour = DateTime.UtcNow.Hour;
+            int currhour = DateTime.Now.Hour;
+
+
+        }
+
+        private void image1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            tabControl.SelectedIndex = 0;
+            
+        }
+
+        private void image2_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            tabControl.SelectedIndex = 1;
         }
     }
 }
